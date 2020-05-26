7c67e5834448f6ecaebd6fd9394e0a7581dfed18
Simon Harrer <simon.harrer@innoq.com>
2020-05-26 21:21:13 +0200

    Bump to v0.0.17

--- Formula/mob.rb
+++ Formula/mob.rb
@@ -1,9 +1,9 @@
 class Mob < Formula
     desc "Swift handover for remote mobs using git. mob is a CLI tool written in GO. It keeps your master branch clean and creates WIP commits on mob-session branch."
     homepage "https://github.com/remotemobprogramming/mob"
-    url "https://github.com/remotemobprogramming/mob/releases/download/v0.0.13/mob_v0.0.13_darwin_amd64.tar.gz"
-    version "v0.0.13"
-    sha256 "da27417a7252a1159d0084ecf6adb81900bd80bae0420bf5526909c728223f3d"
+    url "https://github.com/remotemobprogramming/mob/releases/download/v0.0.17/mob_v0.0.17_darwin_amd64.tar.gz"
+    version "v0.0.17"
+    sha256 "4c20ccb31b1b279e37ae081d996e7f8a58c96bf2e99d24392724a260387c3b76"
   
     def install
       bin.install "mob"
--- README.md
+++ README.md
@@ -16,3 +16,9 @@ For more information, visit https://github.com/remotemobprogramming/mob.
 If you want to learn more about the topic Mob Programming, you may also want to visit https://www.remotemobprogramming.org.
 
 Thanks to https://twitter.com/phaus for the contribution!
+
+## Update Mob
+
+1. Update link and version in `Formula/mob.rb`
+2. Run `brew fetch --force Formula/mob.rb` to determine the new checksum
+3. Update checksum
