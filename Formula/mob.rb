41c450babad6904555cf2c9ef9a136e365d4c621
Helge Biel <18981810+hbiel@users.noreply.github.com>
2021-11-25 21:22:33 +0100

    added linux package support

--- Formula/mob.rb
+++ Formula/mob.rb
@@ -5,6 +5,11 @@ class Mob < Formula
     version "v2.1.0"
     sha256 "0914070256bd5ca93ad928307936489bab0c239455da055085eb528f83144302"
   
+    if OS.linux?
+        url "https://github.com/remotemobprogramming/mob/releases/download/v2.1.0/mob_v2.1.0_linux_amd64.tar.gz"
+        sha256 "0d4a2951e9aced5eb369bdd39ad57cc54e9973db8dfc130d94c14a1d53cf9351"
+    end
+  
     def install
       bin.install "mob"
     end
