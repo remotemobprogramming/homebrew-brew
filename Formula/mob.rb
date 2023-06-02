d652717597059aa3032a171083408aa5c042510f
CI <ci@example.com>
2023-06-02 12:08:32 +0000

    mob v4.4.3

--- Formula/mob.rb
+++ Formula/mob.rb
@@ -1,9 +1,9 @@
 class Mob < Formula
     desc "Swift handover for remote mobs using git. mob is a CLI tool written in GO. It keeps your master branch clean and creates WIP commits on mob-session branch."
     homepage "https://github.com/remotemobprogramming/mob"
-    url "https://github.com/remotemobprogramming/mob/releases/download/v4.4.2/mob_v4.4.2_darwin_universal.tar.gz"
-    version "v4.4.2"
-    sha256 "7f77ffea8ea6c6c7b422e0a1f3c0d6c009ac0bcf496455e26458ec09bcd65698"
+    url "https://github.com/remotemobprogramming/mob/releases/download/v4.4.3/mob_v4.4.3_darwin_universal.tar.gz"
+    version "v4.4.3"
+    sha256 "6e8f670fe16326465396b7f313b1a7092d4b10433e0f4ec5b8a47c00d7ab3306"
   
     def install
       bin.install "mob"
